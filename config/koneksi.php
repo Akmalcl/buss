74fcc8394d41612f92a0434a6001512ab49c2fb6
bagus farouk <b46usf@gmail.com>
2024-04-03 21:17:39 +0700

    commit

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "database";
+$database = "socommerce";
 
 // Mencoba koneksi ke database
 $conn = mysqli_connect($host, $user, $password, $database);
